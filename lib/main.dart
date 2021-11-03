23e77bfeeca97d579ffaf832191d6b8fb1050f8c
e.marquesdev <e.marquesdev@gmail.com>
2021-11-03 16:04:07 -0300

    Entendendo o uso de Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Bem-vindo ao ByteBank', textDirection: TextDirection.ltr, textAlign: TextAlign.center,textScaleFactor: 1.5));
+void main() => runApp(
+      Column(
+        children: const <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+            textAlign: TextAlign.center,
+          ),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.contain, // otherwise the logo will be tiny
+              child: FlutterLogo(),
+            ),
+          ),
+        ],
+      ),
+    );
