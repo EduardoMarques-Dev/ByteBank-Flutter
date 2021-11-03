05dd43f6c63434670c64fc479f7249c3b097e4a4
e.marquesdev <e.marquesdev@gmail.com>
2021-11-03 16:47:23 -0300

    Aula: Implementando o design do item de transferência

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,29 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text("Transferências"),
         ),
+        body: Column(
+          children: const [
+            Card(
+              child: ListTile(
+                title: Text("100.0"),
+                subtitle: Text("Conta: Abc-123"),
+                leading: Icon(Icons.monetization_on),
+              ),
+            ),Card(
+              child: ListTile(
+                title: Text("200.0"),
+                subtitle: Text("Conta: Abc-123"),
+                leading: Icon(Icons.monetization_on),
+              ),
+            ),Card(
+              child: ListTile(
+                title: Text("300.0"),
+                subtitle: Text("Conta: Abc-123"),
+                leading: Icon(Icons.monetization_on),
+              ),
+            ),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: null,
