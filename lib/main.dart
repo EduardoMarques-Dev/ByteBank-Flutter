1d3a700e86a5553b905defaea93cc38e3da75d46
e.marquesdev <e.marquesdev@gmail.com>
2021-11-03 16:33:25 -0300

    Aula: Utilizando o Material Components

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: const <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-            textAlign: TextAlign.center,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-            textAlign: TextAlign.center,
-          ),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain, // otherwise the logo will be tiny
-              child: FlutterLogo(),
-            ),
-          ),
-        ],
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Transferências"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: null,
+        ),
       ),
-    );
+    ));
