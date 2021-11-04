b38b55e44c079e8c84c187658c9630819c7d3220
e.marquesdev <e.marquesdev@gmail.com>
2021-11-04 12:13:22 -0300

    Aula: Conhecendo o hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Transferências"),
-        ),
-        body: ListaTransferencias(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: null,
-        ),
+        body: FormularioTransferencia(),
       ),
-    ));
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Criando Transferência'),
+        ),
+        body: Text('Teste'));
+  }
+}
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget> [
-        ItemTransferencia(Transferencia(100.0,'Conta A')),
-        ItemTransferencia(Transferencia(200.0,'Conta B')),
-        ItemTransferencia(Transferencia(400.0,'Conta D')),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Transferências"),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 'Conta A')),
+          ItemTransferencia(Transferencia(200.0, 'Conta B')),
+          ItemTransferencia(Transferencia(400.0, 'Conta D')),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
@@ -44,9 +67,9 @@ class ItemTransferencia extends StatelessWidget {
   }
 }
 
-  class Transferencia {
-    final double valor;
-    final String Conta;
+class Transferencia {
+  final double valor;
+  final String Conta;
 
-    Transferencia(this.valor, this.Conta);
+  Transferencia(this.valor, this.Conta);
 }
